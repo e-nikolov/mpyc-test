dbf037bfd4c535e24b90ca7a7927dd2aa0949f82
e-nikolov <emil.e.nikolov@gmail.com>
2023-09-12 15:37:15 +0200

    fix index demo paths

--- mpyc-web/vite.config.ts
+++ mpyc-web/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   // root: "../",
-  base: "/mpyc/",
+  base: "/mpyc-web/",
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
