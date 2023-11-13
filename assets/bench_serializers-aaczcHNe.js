88aebe2c6e4ad7221cc2751b78c5f275598dd3f2
e-nikolov <emil.e.nikolov@gmail.com>
2023-11-13 23:34:10 +0100

    Updates

--- assets/bench_serializers-aaczcHNe.js
+++
@@ -1,1 +0,0 @@
-import"./purify.es-qJ_kTSwo.js";import{m as e}from"./index-Qi_Ngyxk.js";import"./index-IqbplxEJ.js";var t={version:"0.6.0",dirty:!0,deployment:"test",timestamp:1699914200029,time:"13/11/2023, 23:23:20",revision:"d09e338"};document.title=`MPyC Web - Bench Serializers (${t.deployment})`;await e();
