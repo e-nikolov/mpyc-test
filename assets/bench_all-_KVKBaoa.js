a0d97925c58394c08dcdc992f11c2b7daeba4dd4
e-nikolov <emil.e.nikolov@gmail.com>
2023-11-14 04:29:19 +0100

    Updates

--- assets/bench_all-_KVKBaoa.js
+++
@@ -1,1 +0,0 @@
-import"./purify.es-qJ_kTSwo.js";import{m as t}from"./index-Qi_Ngyxk.js";import{m as i}from"./index-yjVKgdVf.js";import"./index-IqbplxEJ.js";var e={version:"0.6.0",dirty:!0,deployment:"test",timestamp:1699931151633,time:"14/11/2023, 04:05:51",revision:"819e03f"};document.title=`MPyC Web - Bench All (${e.deployment})`;async function m(){await i(),await t()}await m();
--- assets/bench_serializers-ke3lvsNe.js
+++
@@ -1,1 +0,0 @@
-import"./purify.es-qJ_kTSwo.js";import{m as e}from"./index-Qi_Ngyxk.js";import"./index-IqbplxEJ.js";var t={version:"0.6.0",dirty:!0,deployment:"test",timestamp:1699931151633,time:"14/11/2023, 04:05:51",revision:"819e03f"};document.title=`MPyC Web - Bench Serializers (${t.deployment})`;await e();
