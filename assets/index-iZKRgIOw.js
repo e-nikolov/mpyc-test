26e92a5c4a3d2a5658b2610376eacbd2f720ddb7
e-nikolov <emil.e.nikolov@gmail.com>
2023-12-11 10:47:25 +0100

    Updates

--- assets/index-iZKRgIOw.js
+++
@@ -1,2 +0,0 @@
-import"./modulepreload-polyfill-9p4a8sJU.js";/* empty css              */var e={version:"0.6.0",dirty:!0,deployment:"test",timestamp:1702285063367,time:"11/12/2023, 09:57:43",revision:"b88599f"};document.title=`MPyC Web - Test (${e.deployment})`;
-//# sourceMappingURL=index-iZKRgIOw.js.map
