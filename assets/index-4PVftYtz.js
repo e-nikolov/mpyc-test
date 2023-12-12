b20ede9d5e688d2d73af9f2161dc2eb11715d1d9
e-nikolov <emil.e.nikolov@gmail.com>
2023-12-12 06:47:34 +0100

    Updates

--- assets/index-4PVftYtz.js
+++
@@ -1,2 +0,0 @@
-import"./modulepreload-polyfill-9p4a8sJU.js";/* empty css              */var e={version:"0.9.2",dirty:!0,deployment:"test",timestamp:1702359114064,time:"12/12/2023, 06:31:54",revision:"11976cc"};document.title=`MPyC Web - Bench (${e.deployment})`;
-//# sourceMappingURL=index-4PVftYtz.js.map
---
+++ assets/index-LEqgthtN.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-9p4a8sJU.js";/* empty css              */var e={version:"0.9.2",dirty:!0,deployment:"test",timestamp:1702360039359,time:"12/12/2023, 06:47:19",revision:"11976cc"};document.title=`MPyC Web - Test (${e.deployment})`;
+//# sourceMappingURL=index-LEqgthtN.js.map
